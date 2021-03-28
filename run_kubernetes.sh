31dc84fb57db1b571e3e3771f113176ca9738078
Ankush <ankush@gmail.com>
2021-03-28 14:37:07 +0530

    Kubernetes Build

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=itsdankush/mlproject
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run my-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mongo-75f59d57f4-4nd6q 8000:80
