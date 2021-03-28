539bf6442ad88f36ebb1e063e0cd628e9fd186de
Ankush <ankush@gmail.com>
2021-03-28 14:28:52 +0530

    Vaiable based push error

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,17 +4,15 @@
 # Assumes that an image is built via `run_docker.sh`
 
 #docker login --username=<username>
-#docker tag caafa0609e0c itsdankush/mlproject:firsttry 
+#docker tag caafa0609e0c itsdankush/mlproject:stable 
 #docker push itsdankush/mlproject   
 
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath= itsdankush/mlproject:stable 
-
+dockerpath=itsdankush/mlproject
 # Step 2:  
 # Authenticate & tag
-#docker tag caafa0609e0c itsdankush/mlproject:succesfullbuild 
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
