f64c23a8231cb734fa8ee4ccffadd0b58ae40be5
Ankush <ankush@gmail.com>
2021-03-28 14:21:34 +0530

    Upload docker update

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,20 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
+#docker login --username=<username>
+#docker tag caafa0609e0c itsdankush/mlproject:firsttry 
+#docker push itsdankush/mlproject   
+
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath= itsdankush/mlproject 
 
 # Step 2:  
 # Authenticate & tag
+#docker tag caafa0609e0c itsdankush/mlproject:succesfullbuild 
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push itsdankush/mlproject 
